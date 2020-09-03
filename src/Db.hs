93f578a6337a731cfadd7a6c3ee970bd695bf171
Pavel Zinovev <zombiqwerty@yandex.ru>
2020-09-03 03:36:35 +0300

    Add response types & improve code

--- src/Db.hs
+++ src/Db.hs
@@ -1,6 +1,7 @@
 module Db where
 
 import Control.Exception (SomeException, try)
+import Data.Bifunctor (Bifunctor (bimap))
 import Data.Int
 import Data.Pool (Pool, withResource)
 import Data.Word (Word16)
@@ -8,7 +9,6 @@ import Database.PostgreSQL.Simple
 import qualified Database.PostgreSQL.Simple as P
 import Domain
 import GHC.Generics (Generic)
-import Data.Bifunctor (Bifunctor(bimap))
 
 data DbConfig = DbConfig
   { dbHost :: String,
@@ -30,30 +30,25 @@ newConn conf =
         connectDatabase = dbName conf
       }
 
-tryExec :: IO a -> IO (Either SomeException a)
-tryExec action = do
-  res :: (Either SomeException a) <- try action
-  pure res
-
 --------------------------------------------------------------------------------
 -- Utilities for interacting with the DB.
 -- No transactions.
 --
 -- Accepts arguments
 fetch :: (ToRow q, FromRow r) => Pool P.Connection -> q -> Query -> IO (Either SomeException [r])
-fetch pool args sql = tryExec $ withResource pool retrieve
+fetch pool args sql = try $ withResource pool retrieve
   where
     retrieve conn = query conn sql args
 
 -- No arguments -- just pure sql
 fetchSimple :: (FromRow r) => Pool P.Connection -> Query -> IO (Either SomeException [r])
-fetchSimple pool sql = tryExec $ withResource pool retrieve
+fetchSimple pool sql = try $ withResource pool retrieve
   where
     retrieve conn = query_ conn sql
 
 -- Update database
 execSql :: ToRow q => Pool P.Connection -> q -> Query -> IO (Either SomeException Int64)
-execSql pool args sql = tryExec $ withResource pool ins
+execSql pool args sql = try $ withResource pool ins
   where
     ins conn = execute conn sql args
 
@@ -63,4 +58,3 @@ listArticles :: Pool Connection -> IO (Either SomeException [Article])
 listArticles pool = do
   res <- fetchSimple pool "SELECT * FROM articles ORDER BY id DESC"
   pure $ bimap id (map (\(id, title, content) -> Article id title content)) res
-  
--- src/Handler.hs
+++ src/Handler.hs
@@ -1,17 +1,29 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Handler where
 
-import Control.Exception (Exception (displayException))
+import Control.Exception (Exception (displayException), SomeException)
 import Control.Monad.IO.Class
+import Data.Aeson hiding (json)
+import Data.ByteString.Lazy (ByteString)
 import Data.Pool (Pool)
-import Data.Text.Lazy (pack)
 import Database.PostgreSQL.Simple (Connection)
 import Db
+import GHC.Generics (Generic)
 import Web.Scotty
+
+data ResponseSuccess r = ResponseSuccess {result :: r}
+  deriving (Show, Generic, ToJSON)
+
+data ResponseFailure = ResponseFailure {message :: String}
+  deriving (Show, Generic, ToJSON)
+
+makeResponse :: (ToJSON r, Show r) => Either SomeException r -> ActionM ByteString
+makeResponse d = do
+  setHeader "Content-Type" "application/json"
+  pure $ either (encode . ResponseFailure . displayException) (encode . ResponseSuccess) d
 
 makeHandlers :: Pool Connection -> ScottyM ()
 makeHandlers pool = do
-  get "/articles" $ do
-    articles <- liftIO $ listArticles pool
-    case articles of
-      Left e -> html $ pack $ displayException e
-      Right r -> json r
+  "/articles" `get` (raw =<< makeResponse =<< liftIO (listArticles pool))
+
